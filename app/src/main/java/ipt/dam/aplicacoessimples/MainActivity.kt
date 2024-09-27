b90a2ea32dd7ad626e147baef65911a0df19c716
pauloagsantos <pauloagsantos@gmail.com>
2024-09-27 17:28:50 +0100

    btnDizOla
    botão com evento onclick

--- app/src/main/java/ipt/dam/aplicacoessimples/MainActivity.kt
+++ app/src/main/java/ipt/dam/aplicacoessimples/MainActivity.kt
@@ -1,20 +1,26 @@
 package ipt.dam.aplicacoessimples
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var btnOla: Button = findViewById(R.id.btnDizOla)
+        btnOla.setOnClickListener {
+            dizOla()
         }
+    }
+
+    fun dizOla() {
+        Log.d("Atenção:", "Vou dizer Olá")
+        val txtOla: TextView = findViewById(R.id.txtOla)
+        txtOla.text = "Ola Mundo"
     }
 }
